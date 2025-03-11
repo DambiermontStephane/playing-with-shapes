60fc4acc70001d397373958278d22206a34ffd71
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2025-03-11 09:22:53 +0100

    added settings for circle

---
+++ src/ts/settings.ts
@@ -0,0 +1,22 @@
+export const settings = {
+    swissFlag: {
+        rect: {
+            width: 280,
+            height: 65,
+            backgroundColor: 'white'
+        },
+        backgroundColor: 'red'
+    },
+
+    masterCard: {
+        circle: {
+            r: 360,
+        },
+
+        color: {
+            backgroundColor: 'black',
+            ellipseRed: 'red',
+            ellipseOrange: 'orange',
+        }
+    }
+}
